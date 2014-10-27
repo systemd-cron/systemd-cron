8a4caa71f0ba31ed0958539d87ddd8d382678455
Alexandre Detiste <alexandre.detiste@gmail.com>
2014-10-27 19:50:59 +0100

    send mails in UTF8 (●)

--- src/bin/mail-on-failure.sh
+++ src/bin/mail-on-failure.sh
@@ -6,6 +6,9 @@ MAILTO=$(echo $JOB_ENV | sed -r 's/.*MAILTO=([^"]*).*/\1/')
 
 cat <<EOF | sendmail -i -B8BITMIME "$MAILTO"
 Subject: [$HOSTNAME] job $1 failed
+MIME-Version: 1.0
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
 
 $(systemctl status "$1")
 EOF
