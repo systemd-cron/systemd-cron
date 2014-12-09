34207c1c149fb0c02c85f53725b80bbed7b2f45e
Alexandre Detiste <alexandre.detiste@gmail.com>
2014-12-09 16:09:54 +0100

    first working setuid helper

--- src/bin/crontab_setuid.c
+++ src/bin/crontab_setuid.c
@@ -1,23 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+void end(char * msg){
+	fprintf(stderr,"ERROR: %s,aborting\n", msg);
+	exit(1);
+}
 
 int main(int argc, char *argv[]) {
-	printf("CRONTAB_DIR=%s\n", CRONTAB_DIR);
-	if (argc != 2) {
-		fprintf(stderr,"ERROR\n");
-		return 1;
-	}
+	if (argc != 2) end("bad argument");
+
+	struct passwd *pw;
+	pw = getpwuid(getuid());
+	if (!pw) end("user doesn't exist");
+
+	char crontab[256];
+	snprintf(crontab, sizeof crontab, "%s/%s", CRONTAB_DIR, pw->pw_name);
+	FILE *file;
+
+        char buffer[512];
+
 	switch(argv[1][0]) {
 		case 'r':
-			printf("READ\n");
+			file = fopen(crontab, "r");
+			if (file == NULL) {
+				if(errno == ENOENT) return 0;
+				perror("Cannot open input file");
+				return 1;
+			};
+			while(!feof(file)) {
+				if (fgets(buffer, sizeof(buffer), file))
+					fprintf(stdout, "%s", buffer);
+			}
+			fclose(file);
 			break;
 		case 'w':
-			printf("WRITE\n");
+			file = fopen(crontab, "w");
+			if (file == NULL) {
+				perror("Cannot open output file");
+				return 1;
+			}
+			while(!feof(stdin)) {
+				if (fgets(buffer, sizeof(buffer), stdin))
+					fprintf(file, "%s", buffer);
+			}
+			fclose(file);
 			break;
 		case 'd':
-			printf("DELETE\n");
+			if (unlink(crontab) == -1) {
+				if(errno == ENOENT) return 0;
+				perror("Cannot delete file");
+				return 1;
+			};
 			break;
 		default:
-			fprintf(stderr,"ERROR\n");
+			end("unknown action");
 			return 1;
 	}
 	return 0;
