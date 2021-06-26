2a951b3730187a7140ad41634db0157f05044076
Alexandre Detiste <alexandre.detiste@gmail.com>
2021-06-26 13:19:17 +0200

    merge parts of PR#1 by Trent W. Buck

--- src/bin/mail_on_failure.py
+++ src/bin/mail_on_failure.py
@@ -1,45 +1,58 @@
 #!/usr/bin/python3
-import sys
+import argparse
+import logging
 import os
 import subprocess
 from subprocess import Popen, PIPE
 
-try:
-    job = sys.argv[1]
-except IndexError:
-    sys.exit("Usage: %s <unit>" % sys.argv[0])
+__DOC__ = """ send a panic email about a failed cron job """
+
+parser = argparse.ArgumentParser(description=__DOC__)
+parser.add_argument('unit', help='the failing unit, e.g. cron-foo-1.service')
+parser.add_argument('--verbose', action='store_true')
+args = parser.parse_args()
+if args.verbose:
+    logging.getLogger().setLevel(logging.INFO)
 
 for pgm in ('/usr/sbin/sendmail', '/usr/lib/sendmail'):
     if os.path.exists(pgm):
         break
 else:
-    print("<3>can't send error mail for %s without a MTA" % job)
+    print("<3>can't send error mail for %s without a MTA" % args.unit)
     exit(0)
 
-user = subprocess.check_output(['systemctl','show',job,'--property=User'], universal_newlines=True)
+user = subprocess.check_output(
+                     ['systemctl', 'show', args.unit, '--property=User'],
+                     universal_newlines=True)
 user = user.rstrip('\n').split('=')[1]
-if not user: user = 'root'
+if not user:
+    user = 'root'
 
 mailto = user
 
-job_env = subprocess.check_output(['systemctl','show',job,'--property=Environment'], universal_newlines=True)
+job_env = subprocess.check_output(
+                        ['systemctl', 'show', args.unit, '--property=Environment'],
+                        universal_newlines=True)
 job_env = job_env.rstrip('\n').split('=', 1)[1]
 
 if job_env:
     for var in job_env.split(' '):
         try:
             key , value = var.split('=', 1)
-            if key == 'MAILTO': mailto = value
+            if key == 'MAILTO':
+                mailto = value
         except ValueError:
             pass
 
-if not mailto: exit(0)
+if not mailto:
+   logging.info('This cron job (%s) opted out of email, therefore quitting', args.unit)
+   exit(0)
 
 hostname = os.uname()[1]
 
 body = "From: root (systemd-cron)\n"
 body += "To: " + mailto + "\n"
-body += "Subject: [" + hostname + "] job " + job  + " failed\n"
+body += "Subject: [" + hostname + "] job " + args.unit  + " failed\n"
 body += "MIME-Version: 1.0\n"
 body += "Content-Type: text/plain; charset=UTF-8\n"
 body += "Content-Transfer-Encoding: 8bit\n"
@@ -50,10 +63,9 @@ for locale in (None, 'C.UTF-8', 'C'):
     if locale:
         os.environ['LC_ALL'] = locale
     try:
-        systemctl = subprocess.check_output(['systemctl', 'status', job], universal_newlines=True)
+        subprocess.check_output(['systemctl', 'status', args.unit], universal_newlines=True)
     except UnicodeDecodeError:
-        # current locale is broken, try again
-        pass
+        logging.info('current locale (%s) is broken, try again', locale)
     except subprocess.CalledProcessError as e:
         if e.returncode != 3:
             raise
