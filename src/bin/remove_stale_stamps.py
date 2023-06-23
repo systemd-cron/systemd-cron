69a5eea4ba720b50c8a089614a906958b916ccd1
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-06-23 14:48:29 +0200

    PEP8

--- src/bin/remove_stale_stamps.py
+++ src/bin/remove_stale_stamps.py
@@ -3,20 +3,21 @@ import glob
 import time
 import os
 
-actual_stamps=glob.glob('/var/lib/systemd/timers/stamp-cron-*.timer')
+actual_stamps = glob.glob('/var/lib/systemd/timers/stamp-cron-*.timer')
 
-timers=glob.glob('/run/systemd/generator/cron-*.timer')
-needed_stamps=['/var/lib/systemd/timers/stamp-cron-daily.timer',
-               '/var/lib/systemd/timers/stamp-cron-weekly.timer',
-               '/var/lib/systemd/timers/stamp-cron-monthly.timer',
-               '/var/lib/systemd/timers/stamp-cron-quarterly.timer',
-               '/var/lib/systemd/timers/stamp-cron-semi-annually.timer',
-               '/var/lib/systemd/timers/stamp-cron-yearly.timer']
+timers = glob.glob('/run/systemd/generator/cron-*.timer')
+needed_stamps = ['/var/lib/systemd/timers/stamp-cron-daily.timer',
+                 '/var/lib/systemd/timers/stamp-cron-weekly.timer',
+                 '/var/lib/systemd/timers/stamp-cron-monthly.timer',
+                 '/var/lib/systemd/timers/stamp-cron-quarterly.timer',
+                 '/var/lib/systemd/timers/stamp-cron-semi-annually.timer',
+                 '/var/lib/systemd/timers/stamp-cron-yearly.timer']
+
 for timer in timers:
     needed_stamps.append(timer.replace('/run/systemd/generator/cron-',
                                        '/var/lib/systemd/timers/stamp-cron-'))
 
-stale_stamps=set(actual_stamps) - set(needed_stamps)
+stale_stamps = set(actual_stamps) - set(needed_stamps)
 
 now = time.time()
 for stale_stamp in stale_stamps:
