2e9c6d97579c34845d531f394a85c7e83c9ba79c
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-07-29 15:26:41 +0200

    add more tests

--- test/test.py
+++ test/test.py
@@ -11,6 +11,13 @@ def m():
 
 class TestStringMethods(unittest.TestCase):
 
+    def test_path_expansion(self):
+        j = m().Job('-', '@daily dummy true')
+        j.parse_crontab_at(withuser=True)
+        j.generate_unit_name(iter((1,)))
+        j.generate_scriptlet()
+        self.assertEqual(j.execstart, '/usr/bin/true')
+
     def test_period_basic(self):
         j = m().Job('-', '@daily dummy true')
         j.parse_crontab_at(withuser=True)
@@ -22,6 +29,12 @@ class TestStringMethods(unittest.TestCase):
         j.parse_crontab_timespec(withuser=True)
         j.generate_schedule()
         self.assertEqual(j.schedule, '*-*-* 6:5:00')
+
+    def test_timespec_slice(self):
+        j = m().Job('-', '*/5 * * * * dummy true')
+        j.parse_crontab_timespec(withuser=True)
+        j.generate_schedule()
+        self.assertEqual(j.schedule, '*-*-* *:0,5,10,15,20,25,30,35,40,45,50,55:00')
 
     def test_timespec_range(self):
         j = m().Job('-', '1 * * * mon-wed dummy true')
