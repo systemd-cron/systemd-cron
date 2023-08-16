7e76d266d68a106260028cc633fb14f2bb927840
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-08-16 02:47:38 +0200

    r_s_s: simple version

--- src/bin/remove_stale_stamps.sh
+++ src/bin/remove_stale_stamps.sh
@@ -1,27 +1,20 @@
 #!/bin/sh
-LC_ALL=C
-TZ=UTC0
-export LC_ALL TZ
+set -e
+set -u
 
-trap 'rm -rf "$tmpdir"' EXIT INT
-tmpdir="$(mktemp -dt "remove_stale_stamps.XXXXXXXXX")/"
+find /var/lib/systemd/timers/ -name 'stamp-cron-*' -mtime +10 | while read -r stamp
+do
+    timer=${stamp##*/stamp-}
 
-printf '%s\n' /var/lib/systemd/timers/stamp-cron-*.timer > "${tmpdir}actual_stamps"
-
-printf '%s\n' /var/lib/systemd/timers/stamp-cron-daily.timer \
-              /var/lib/systemd/timers/stamp-cron-weekly.timer \
-              /var/lib/systemd/timers/stamp-cron-monthly.timer \
-              /var/lib/systemd/timers/stamp-cron-quarterly.timer \
-              /var/lib/systemd/timers/stamp-cron-semi-annually.timer \
-              /var/lib/systemd/timers/stamp-cron-yearly.timer \
-              /run/systemd/generator/cron-*.timer |
-	sed 's:/run/systemd/generator/cron-:/var/lib/systemd/timers/stamp-cron-:' |
-	sort > "${tmpdir}needed_stamps"
-
-now="$(date +%s)"
-
-comm -23 "${tmpdir}actual_stamps" "${tmpdir}needed_stamps" | while read -r stamp; do
-	mtime="$(stat -c'%Y' "$stamp")"
-	[ "$mtime" -lt $(( now - 10 * 86400 )) ] && rm -f "$stamp" 2>/dev/null
+    if test -f "/run/systemd/generator/$timer"
+    then
+        :
+    elif test -f "/lib/systemd/system/$timer"
+    then
+        :
+    else
+        rm -f "$stamp"
+    fi
 done
-:
+
+exit 0
